85ac96baa155980e0ef2c0ed7574fe356f563a18
Jay <moran.j@pm.me>
2019-04-17 22:32:40 -0700

    Partial Compiler

--- projects/10/Compiler/src/Compiler/CompilationEngine.java
+++ projects/10/Compiler/src/Compiler/CompilationEngine.java
@@ -1,7 +1,90 @@
 package Compiler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CompilationEngine {
-    public CompilationEngine() {
+    PrintWriter out;
+    public CompilationEngine(PrintWriter p) {
+        this.out = p;
+        // try {
+        //     out = new PrintWriter(new BufferedWriter(new FileWriter("MyProg.xml")));
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        CompileClass();
+    }
+
+    public void CompileClass() {
+        JackTokenizer jt = new JackTokenizer("MyProg.jack");
+        out.println("<class>");
+        while (jt.hasMoreTokens()) {
+            if (jt.tokenType() == TokenType.KEYWORD) {
+
+            } else if (jt.tokenType() == TokenType.IDENTIFIER) {
+
+            } else if (jt.tokenType() == TokenType.SYMBOL) {
+
+            } else if (jt.tokenType() == TokenType.STRING_CONST) {
+
+            } else if (jt.tokenType() == TokenType.INT_CONST) {
+
+            }
+        }
+        out.println("</class>");
+    }
+
+    public void CompileClassVarDec() {
+
+    }
+
+    public void CompileSubroutine() {
+
+    }
+
+    public void CompileParameterList() {
+
+    }
+
+    public void compileVarDec() {
+
+    }
+
+    public void compileStatements() {
+
+    }
+
+    public void compileDo() {
+
+    }
+
+    public void compileLet() {
+
+    }
+
+    public void compileWhile() {
+
+    }
+
+    public void compileReturn() {
+
+    }
+
+    public void compileIf() {
+
+    }
+
+    public void CompileExpression() {
+
+    }
+
+    public void CompileTerm() {
+
+    }
+
+    public void CompileExpressionList() {
 
     }
 }
--- projects/10/Compiler/src/Compiler/JackAnalyzer.java
+++ projects/10/Compiler/src/Compiler/JackAnalyzer.java
@@ -3,4 +3,17 @@ package Compiler;
 import java.io.*;
 
 public class JackAnalyzer {
+    public static void main(String[] args) {
+        try {
+            // Create a JackTokenizer from the Xxx.jack input file
+            BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+            JackTokenizer jt = new JackTokenizer(reader);
+            // Create an output file called Xxx.xml and prepare it for writing
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("MyProg.xml")));
+            // Use the CompilationEngine to compile the input JackTokenizer into the output file
+            CompilationEngine ce = new CompilationEngine(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
